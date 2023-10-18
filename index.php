46bebf3d836f0b8bf4b786613b1a4022839b4389
bamagid <bamagid60@gmail.com>
2023-10-18 18:40:00 +0000

    desactivation de l'autocompletion

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
                 <button type="submit" class="up">continuer avec Facebook</button><br>
                 <hr class="hr1"><p class="p2">ou</p><hr class="hr2">
                 <label for="email" class="label">EMAIL: </label><br>
-                <input type="email" name="email" class="entrer" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required> <br>
+                <input type="email" name="email" autocomplete="off" class="entrer" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required> <br>
                 <label for="password" class="label">PASSWORD: </label><br>
-                <input type="password" name="password" class="entrer1" required><br>
+                <input type="password" name="password" autocomplete="off" class="entrer1" required><br>
                 <div class="foot_form">
-                    <b><a href="inscription.html">Je n'ai pas de compte</a></b>
+                    <b><a href="#">Je n'ai pas de compte</a></b>
                     <button type="submit" name="connect" class="down">Se connecter</button>
                 </div>
             </form>
@@ -29,17 +29,17 @@
                 <div class="pre_nom">
                    <div class="prenom"> 
                     <label for="prenom" class="lab_pre"> PRENOM:</label><br>
-                    <input type="text" placeholder="Nadia" name="prenom" class="entrerp" pattern="[a-zA-Zàéùè -]{2,50}" required><br>
+                    <input type="text" placeholder="Nadia" name="prenom" class="entrerp" autocomplete="off" pattern="[a-zA-Zàéùè -]{2,50}" required><br>
                    </div>
                    <div class="nom">
                     <label for="nom">NOM:</label><br>
-                    <input type="text" placeholder="Abderahim" name="nom" pattern="[a-zA-Z]{2,30}" class="entrern" required><br>
+                    <input type="text" placeholder="Abderahim" name="nom" pattern="[a-zA-Z]{2,30}" autocomplete="off" class="entrern" required><br>
                    </div>
                 </div>
                 <label for="telephone">TELEPHONE:</label><br>
-                <input type="tel" placeholder="75 350 10 30" name="telephone" class="telephone" pattern="^7[05768]{1}+[0-9]{7}$" required><br>
+                <input type="tel" placeholder="75 350 10 30" name="telephone" class="telephone" autocomplete="off" pattern="^7[05768]{1}+[0-9]{7}$" required><br>
                 <label for="telephone">EMAIL:</label><br>
-                <input type="email" placeholder="example@gmail.com" name="email2" class="entrer" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required><br>
+                <input type="email" placeholder="example@gmail.com" name="email2" class="entrer" autocomplete="off" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required><br>
                 <label for="telephone">PASSWORD:</label><br>
                 <input type="password" name="password2" class="entrer" required><br>
                 <div class="gift">
