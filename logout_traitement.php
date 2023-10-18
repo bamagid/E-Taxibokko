9244cf8a73c5b40e6405e109d5fea201ca7417aa
bamagid <bamagid60@gmail.com>
2023-10-18 16:57:54 +0000

    ajout de l'affichage de tous les utilisateurs inscrit sur la plateforme lors ce q'un utilisateur arrive a s'inscrire

--- logout_traitement.php
+++ logout_traitement.php
@@ -6,8 +6,7 @@ if (isset($_POST['logout']) && !empty($_POST['nom']) && !empty($_POST['prenom'])
     $prenom=htmlspecialchars($_POST['prenom']);
     $tel=htmlspecialchars($_POST['telephone']);
     $mail=htmlspecialchars($_POST['email2']);
-    $pswd=htmlspecialchars($_POST['password2']);
-    // $mpd=hash('sha1',$pswd);
+    $pswd=($_POST['password2']);
     $insert= $bdd->prepare('INSERT INTO taxiboko (nom,prenom,telephone,email,password) VALUES (:nom,:prenom,:telephone,:email,:password)');
     $insert->execute(array(
     'nom'=>$nom,
@@ -15,5 +14,49 @@ if (isset($_POST['logout']) && !empty($_POST['nom']) && !empty($_POST['prenom'])
     'telephone'=>$tel,
     'email'=>$mail,
     'password'=>$pswd));
-       
-}
+    $verif=$bdd->prepare("SELECT nom,prenom,telephone,email,date FROM taxiboko WHERE email='$mail' AND password ='$pswd' ");
+    $verif->execute();
+        if($verif->rowCount() !=0){
+            $ins=$verif->fetch();
+            // foreach ($ins as $cles=>$value) {
+               echo"<P> Bravo ". $ins['nom']." ".$ins['prenom']." votre inscription chez e-taxiboko a reussi! </p> <br><br> ";
+            // }
+            $user=$bdd->prepare('SELECT nom,prenom,telephone,date FROM taxiboko ');
+            $user->execute();
+            echo "<P>La liste des personnes inscrits sur la plateforme est la suivante:</p><br>";
+            echo "<table>";
+                echo "<th>";
+                echo"<P>  NOM  </p>";
+                echo "</th>";
+                echo "<th>";
+                echo"<P>  PRENOM  </p> ";
+                echo "</th>";
+                echo "<th";
+                echo "<P>   TELEPHONE   </p> ";
+                echo "</th>";
+                echo "<th>";
+                echo "<P>   DATE D'INSCRIPTION    </p>";
+                echo "</th>";
+             echo "</table>";
+            foreach ($user as $value) {
+                echo "<table>";
+                echo "<td>";
+                echo"<P> ". $value['nom']." </p>";
+                echo "</td";
+                echo "<td>";
+                echo"<P> ". $value['prenom']." </p> ";
+                echo "</td>";
+                echo "<td>";
+                echo "<P> ".$value['telephone']." </p> ";
+                echo "</td>";
+                echo "<td>";
+                echo "<P>    ".$value['date']."     </p>";
+                echo "</td>";
+                echo "</table>";
+
+                
+            }
+        }else{
+           echo "votre inscrition a echouer veuillez reesayer avec de bonnes informations";
+        }
+    }
