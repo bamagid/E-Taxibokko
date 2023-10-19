fd10f67c9ff88586039450165d087bf35ca0dadf
bamagid <bamagid60@gmail.com>
2023-10-19 13:01:42 +0000

    ajout de commentaires pour mieux documenter mon code ensuite j'ai ajouter une session qui va conternir la liste des utilisateur qui se trouve sur la base de données ensuite lorsque l'utilisateur a passer les validation pour la connexion je lui ai rediriger vers la page 'user_list.php'

--- connexion.php
+++ connexion.php
@@ -1,20 +1,23 @@
 <?php
 session_start();
-require_once'config.php';
+require_once 'config.php';
+//Verifier que les champs du formuliares ne sont pas vide et que l'utilisateur a cliquer sur le bouton avec le nom 'connect'
 if (isset($_POST['connect']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-   $email=htmlspecialchars($_POST['email']);
-   $pass=htmlspecialchars($_POST['password']);
-   $query = $bdd->prepare("SELECT * FROM taxiboko WHERE email = '$email' AND password = '$pass'");
-   $query->execute();
-   if ($query->rowCount()==1) {
-    $tab=$query->fetch();
-    foreach($tab as$tabl){
-     $nom=$tab['nom'];
-     echo "\n Bravo la connexion a reussi bienvenue a e-taxiboko $nom ! ";
-     break;
+    $email = htmlspecialchars($_POST['email']);
+    $pass = htmlspecialchars($_POST['password']);
+    //Faire une requete preparée et l'executer
+    $query = $bdd->prepare("SELECT nom, prenom, telephone FROM taxiboko WHERE email = :email AND password = :password");
+    $query->execute(array('email' => $email, 'password' => $pass));
+    //Verifier si la requete a renvoyer une ligne pour confirmer que l'utilisateur existe dans la BDD
+    if ($query->rowCount() == 1) {
+        $user = $query->fetch();
+        // Stocker les données utilisateur dans une session "user"
+        $_SESSION['user'] = $user; 
+        //rediriger l'utilisateur connecter dans la page 'user_list.php'
+        header("Location:user_list.php");
+        exit();
+    } else {
+        echo "Désolé, les identifiants que vous avez entrés sont incorrects.";
     }
-   }else{
-    echo"\n Desolé les identifiants que vous avez entrer sont incorrects ";
-   }
-
 }
+?>
